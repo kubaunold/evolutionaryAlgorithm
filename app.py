05a858d479a5561ec3ef305af8bac545e24dc9bc
Kuba Unold <241160@student.pwr.edu.pl>
2021-05-08 20:55:24 +0200

    Added simple exec and Generate button.

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
 # app.py
 
 import PySimpleGUI as sg
+from sympy import symbols   # for symbolic math
 
-
+# n <= 5
+x1, x2, x3, x4, x5 = symbols('x1 x2 x3 x4 x5')
 
 
 # first column of a layout
 function_definition_column = [
     [
         sg.Text("f(xi)="), 
-		sg.In(size=(25, 1), enable_events=True, key="-FUNCTION-"),
+		sg.In(size=(25, 1), enable_events=True, key="-FUNCTION-")
     ],
     [
-        sg.Text("Kostka"), sg.Text("Ograniczenia"), 
+        sg.Text("Kostka"), sg.Text("Ograniczenia")
+    ],
+    [
+        sg.Button("Generuj", key="-GENERATE-")
     ],
 ]
 
 graph_viewer_column = [
 	[sg.Text("Write a function and click generate:")],
-	[sg.Image(key="-GRAPH-")],
+	[sg.Image(key="-GRAPH-")]
 ]
 
 # ----- Full layout -----
@@ -40,8 +45,12 @@ while True:
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
     if event == "-FUNCTION-":
+        pass
+
+    if event == "-GENERATE-":
         folder = values["-FUNCTION-"]
-        print(folder)
+        print(eval(folder))
+
 
 
 window.close()
